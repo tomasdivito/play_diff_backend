2937379709e73a78a4240077abe99d8a15214c81
Tomas Di Vito <tomasdv2@gmail.com>
2019-06-25 18:32:46 -0300

    deleted modules ignored them + implemented basic login working with front

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const express = require('express')
+const querystring = require('querystring')
+const request = require('request')
 
 const PORT = process.env.PORT || 8080
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 
 const app = express()
 
@@ -14,20 +17,46 @@ app.get('/login', function (req, res) {
     'playlist-modify-public',
     'playlist-modify-private'
   ]
-  const SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize'
-  const SCOPES = encodeURIComponent(SPOTIFY_SCOPES.join(' '))
-  const REDIRECT_URI = encodeURIComponent('http://localhost:8080/auth')
+  const SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize?'
+  const REDIRECT_URI = 'http://localhost:8081/callback'
 
-  res.redirect(`${SPOTIFY_AUTH_URL}?response_type=code&client_id=${CLIENT_ID}&scope=${SCOPES}&redirect_uri=${REDIRECT_URI}`)
+  res.redirect(SPOTIFY_AUTH_URL + querystring.stringify({
+    response_type: 'code',
+    client_id: CLIENT_ID,
+    scope: SPOTIFY_SCOPES.join(' '),
+    redirect_uri: REDIRECT_URI
+  }))
 })
 
-app.get('/logged_in', function (req, res) {
-  console.log('req:', req)
-  console.log('res:', res)
+app.get('/callback', function (req, res) {
+  const code = req.query.code
+  console.log('callback called')
 
-  res.send("Correctly logged in with spotify!!!")
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: 'http://localhost:8081/callback',
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64'))
+    },
+    json: true
+  }
+
+  request.post(authOptions, function (error, response, body) {
+    console.log(body);
+    const ACCESS_TOKEN = body.access_token
+    const REFRESH_TOKEN = body.refresh_token
+
+    console.log('access-token:', ACCESS_TOKEN)
+
+    res.redirect('http://localhost:3000/#' + querystring.stringify({
+      access_token: ACCESS_TOKEN,
+      refresh_token: REFRESH_TOKEN
+    }))
+  })
 })
-
-app.get('/', (req, res) => res.send("Hello world"))
 
 app.listen(PORT, () => console.log(`App running and listening on port ${PORT}`))
